590fe21ebb0295a6c8bc5eff7e07ac3649151cf2
sinisa.miskovic@sattrakt.com <sinisa.miskovic@sattrakt.com>
2022-06-30 16:01:59 +0200

    change character schema, added example json

--- models/Character.js
+++ models/Character.js
@@ -1,13 +1,67 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const CharacterSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, 'Name is required']
-    }
-});
-
+  api_id: Number,
+  name: String,
+  description: String,
+  modified: Date,
+  thumbnail: {
+    path: String,
+    extension: String,
+  },
+  resourceURI: String,
+  comics: {
+    available: Number,
+    collectionURI: String,
+    items: [
+      {
+        resourceURI: String,
+        name: String,
+      },
+    ],
+    returned: Number,
+  },
+  series: {
+    available: Number,
+    collectionURI: String,
+    items: [
+      {
+        resourceURI: String,
+        name: String,
+      },
+    ],
+    returned: Number,
+  },
+  stories: {
+    available: Number,
+    collectionURI: String,
+    items: [
+      {
+        resourceURI: String,
+        name: String,
+      },
+    ],
+    returned: Number,
+  },
+  events: {
+    available: Number,
+    collectionURI: String,
+    items: [
+      {
+        resourceURI: String,
+        name: String,
+      },
+    ],
+    returned: Number,
+  },
+  urls: [
+    {
+      type:{type:String},
+      uri: String,
+    },
+  ],
+}, { typeKey: '$type' });
 
 const Character = mongoose.model("Character", CharacterSchema);
 
